f6a042029201b118aa63521a74ecb79ff8094d63
Filip Prochazka <minebox573@gmail.com>
2024-12-18 17:58:27 +0100

    CPP: #1 Two Sum

---
+++ cpp/1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> map;
+    
+    for (int i = 0; i < (int)nums.size(); i++) {
+        if (map.contains(target - nums[i])) 
+            return {map[target - nums[i]], i};
+        
+        map[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2,7,11,15};
+
+    for (auto i : twoSum(nums, 9)) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
