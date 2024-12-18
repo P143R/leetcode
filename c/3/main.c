8f6c46d16ff22573a63118351f14dda673608ecb
Filip Prochazka <minebox573@gmail.com>
2024-12-18 17:57:44 +0100

    C: #3 Longest Substring Without Repeating Characters

---
+++ c/3/main.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int lengthOfLongestSubstring(char *right) {
+    unsigned sequence[4] = {0, 0, 0, 0};
+    int maxCount = 0;
+    int count = 0;
+
+    char *left = right;
+
+    while (*right != '\0') {
+        while (sequence[*right / 32] & (unsigned)1 << (*right % 32)) {
+            sequence[*left / 32] ^= (unsigned)1 << (*left % 32);
+            left++;
+            count--;
+        }
+
+        sequence[*right / 32] ^= (unsigned)1 << (*right % 32);
+
+        if (maxCount < ++count)
+            maxCount = count;
+
+        right++;
+    }
+
+    return maxCount;
+}
+
+int main() {
+    
+    printf("%d\n", lengthOfLongestSubstring((char *)"abcabcbb"));
+    return 0;
+}
