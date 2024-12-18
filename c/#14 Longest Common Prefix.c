4da189f34facafeca375a02b3d573ffeaec1ec6c
Filip Prochazka <minebox573@gmail.com>
2024-12-18 23:14:57 +0100

    C: #14 Longest Common Prefix

---
+++ c/#14 Longest Common Prefix.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+char *longestCommonPrefix(char **strs, int strsSize) {
+    char *res = *strs;
+
+    while (*res != '\0') {
+        for (int i = 1; i < strsSize; i++) {
+            if (*res != *strs[i]) {
+                *res = '\0';                
+                return *strs;
+            }
+
+            strs[i]++;
+        }
+        
+        res++;
+    }
+
+    return *strs;
+}
+
+int main() {
+    char str1[] = "flower";
+    char str2[] = "flow";
+    char str3[] = "flight";
+    char *s[] = {str1, str2, str3};
+
+    char *lcp = longestCommonPrefix(s, 3);
+    printf("%s\n", lcp);
+
+    return 0;
+}
