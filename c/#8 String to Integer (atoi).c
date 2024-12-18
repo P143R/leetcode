b99510f14878eeafd02d03118b23a6409b73088b
Filip Prochazka <minebox573@gmail.com>
2024-12-19 00:45:43 +0100

    C: #8 String to Integer (atoi)

---
+++ c/#8 String to Integer (atoi).c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX INT_MAX / 10
+
+int myAtoi(char *s) {
+    while (isspace(*s))
+        s++;
+
+    int negative = 1;
+
+    if (*s == '-') {
+        negative = -1;
+        s++;
+    } else if (*s == '+')
+        s++;
+
+    if (!isdigit(*s))
+        return 0;
+
+    int res = *(s++) - '0';
+
+    while (isdigit(*s)) {
+        int digit = (*s - '0') * negative;
+
+        if (res > MAX ||
+                (res == MAX && (digit <= -8 || digit >= 7)))
+            return negative == -1 ? INT_MIN : INT_MAX;
+
+        res *= 10;
+        res += *(s++) - '0';
+    }
+
+    return res * negative;
+}
+
+int main() { return 0; }
